1be27f9178c14b35b546eee03b36bfc68bedae5b
tajmin <tajminur.aiub@gmail.com>
2021-10-10 20:30:19 +0600

    update - whatever it is

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
@@ -9,8 +10,10 @@ import Shop from './components/Shop/Shop';
 import SuccessfulPurchase from './components/SuccessfulPurchase/SuccessfulPurchase';
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
-    <div>
+    <div className={isDark ? "dark-theme" : ''}>
+      <button onClick={() => setIsDark(!isDark)}>Toggle Dark</button>
       <BrowserRouter>
         <Header></Header>
         <Switch>
